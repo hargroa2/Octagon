d51b5e4dd4a236baa675857fd7eaf04a0275a800
Amanda Hargrove <amandahargrove98@gmail.com>
2022-07-11 16:55:39 -0400

    Uploading progress, specifically with rankings.js

--- JavaScript/rankings.js
+++ JavaScript/rankings.js
@@ -19,6 +19,7 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 
 //====== API FETCH FOR RANKINGS ====== COMMENT
 const rankButton = document.getElementById("rankingsData");
+const rankings = document.getElementById("ranks");
 
 const options = {
   method: "GET",
@@ -34,5 +35,65 @@ const ufcRankings = async () => {
   const ufcData = await fetch(url, options);
   const json = await ufcData.json();
   console.log(json);
+
+  // ====== APPENDING DATA TO PAGE ====== COMMENT
+
+  // const list = document.getElementById("list");
+  const rank = document.getElementById("ranks");
+  // const num = document.getElementById("number");
+  // const name = document.getElementById("name");
+  // const link = document.getElementById("link");
+
+  const forEachClass = (index) => {
+    for (let fighter of Object.keys(json[0].fighters)) {
+      // append the P4P contents onto the page
+      //NOTE: May need to change this later for css reasons
+      const paragraph = document.createElement("p");
+      paragraph.innerText = json[index].fighters[fighter].fighter_ranking;
+      rank.append(paragraph);
+
+      const paragraphNames = document.createElement("p");
+      paragraphNames.innerText = json[index].fighters[fighter].fullName;
+      rank.append(paragraphNames);
+
+      const paragraphLinks = document.createElement("p");
+      paragraphLinks.innerText = json[index].fighters[fighter].url;
+      rank.append(paragraphLinks);
+    }
+  };
+
+  const weightChoice = () => {
+    if (select.options[select.selectedIndex].value === "P4P") {
+      forEachClass(0);
+    } else if (select.options[select.selectedIndex].value === "FW") {
+      forEachClass(1);
+    } else if (select.options[select.selectedIndex].value === "BW") {
+      forEachClass(2);
+    } else if (select.options[select.selectedIndex].value === "FeW") {
+      forEachClass(3);
+    } else if (select.options[select.selectedIndex].value === "LW") {
+      forEachClass(4);
+    } else if (select.options[select.selectedIndex].value === "WW") {
+      forEachClass(5);
+    } else if (select.options[select.selectedIndex].value === "MW") {
+      forEachClass(6);
+    } else if (select.options[select.selectedIndex].value === "LHW") {
+      forEachClass(7);
+    } else if (select.options[select.selectedIndex].value === "HW") {
+      forEachClass(8);
+    } else if (select.options[select.selectedIndex].value === "WP4P") {
+      forEachClass(9);
+    } else if (select.options[select.selectedIndex].value === "WS") {
+      forEachClass(10);
+    } else if (select.options[select.selectedIndex].value === "WF") {
+      forEachClass(11);
+    } else if (select.options[select.selectedIndex].value === "WB") {
+      forEachClass(12);
+    } else if (select.options[select.selectedIndex].value === "WFe") {
+      forEachClass(13);
+    }
+  };
+
+  weightChoice();
 };
 rankButton.onclick = () => ufcRankings();
