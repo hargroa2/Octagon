b3a0b133ec2825ac9b70cd27d80defe610e17d07
Amanda Hargrove <amandahargrove98@gmail.com>
2022-07-12 00:11:55 -0400

    Upload progress for the day

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,5 @@
+const newsKey = config.NEWS_API_KEY;
+
 // ====== NAVIGATION ====== COMMENT
 const hamburger = document.querySelector(".hamburger");
 
@@ -15,4 +17,38 @@ document.querySelectorAll(".nav-link").forEach((n) =>
   })
 );
 
-//news
+//====== FETCH API DATA ======
+const ufcNewsArticles = async () => {
+  API_KEY = newsKey;
+  const url = `https://newsapi.org/v2/everything?q=UFC&apiKey=${API_KEY}`;
+
+  const ufcNews = await fetch(url);
+  const json = await ufcNews.json();
+  console.log(json);
+
+  // ====== APPEND NEWS TO PAGE ======
+  const newsBox = document.getElementById("newsContainer");
+
+  for (let i = 0; i < 5; i++) {
+    const article = document.createElement("p");
+    const image = document.createElement("img");
+    const linkToArticle = document.createElement("a");
+    const source = document.createElement("p");
+
+    article.innerText = json.articles[i].title;
+
+    image.setAttribute("src", json.articles[i].urlToImage);
+    image.style.height = "200px";
+    image.style.width = "400px";
+
+    linkToArticle.setAttribute("href", json.articles[i].url);
+    linkToArticle.innerText = "Click here for the article";
+
+    source.innerText = json.articles[i].source.name;
+
+    newsBox.append(article, image, source, linkToArticle);
+  }
+};
+
+//ONLY MAKE THIS WORK IF YOU WANT THE API TO BE CALLED
+// ufcNewsArticles();
