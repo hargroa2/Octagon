e6342ee969ca84b986817e40e49d7d7cd5e5d80e
Amanda Hargrove <amandahargrove98@gmail.com>
2022-07-14 15:41:20 -0400

    Uploading progress so far
    Complete home page overhaul, styling other pages

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,3 @@
-const newsKey = config.NEWS_API_KEY;
-
 // ====== NAVIGATION ====== COMMENT
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
@@ -36,49 +34,13 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 // };
 
 //====== JQUERY ====== COMMENT
-
-$(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 900) {
-      $("nav").addClass("changeColor");
-    } else {
-      $("nav").removeClass("changeColor");
-    }
-  });
-});
-
-//====== FETCH API DATA ======
-const ufcNewsArticles = async () => {
-  API_KEY = newsKey;
-  const url = `https://newsapi.org/v2/everything?q=UFC&apiKey=${API_KEY}`;
-
-  const ufcNews = await fetch(url);
-  const json = await ufcNews.json();
-  console.log(json);
-
-  // ====== APPEND NEWS TO PAGE ======
-  const newsBox = document.getElementById("newsContainer");
-
-  for (let i = 0; i < 5; i++) {
-    const article = document.createElement("p");
-    const image = document.createElement("img");
-    const linkToArticle = document.createElement("a");
-    const source = document.createElement("p");
-
-    article.innerText = json.articles[i].title;
-
-    image.setAttribute("src", json.articles[i].urlToImage);
-    image.style.height = "200px";
-    image.style.width = "400px";
-
-    linkToArticle.setAttribute("href", json.articles[i].url);
-    linkToArticle.innerText = "Click here for the article";
-
-    source.innerText = json.articles[i].source.name;
-
-    newsBox.append(article, image, source, linkToArticle);
-  }
-};
-
-//ONLY MAKE THIS WORK IF YOU WANT THE API TO BE CALLED
-// ufcNewsArticles();
+//Changes background colored nav bar when scrolling down
+// $(function () {
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 50) {
+//       $("nav").addClass("changeColor");
+//     } else {
+//       $("nav").removeClass("changeColor");
+//     }
+//   });
+// });
